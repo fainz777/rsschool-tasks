c5e73c22842716120efd8e73a621e386d1cd4e4f
Oksana Fainitskaia <of@devinform.ru>
2019-11-04 13:10:13 +0300

    feat: virtual keyboard lang switching

--- codejam-virtual-keyboard/src/js/index.js
+++ codejam-virtual-keyboard/src/js/index.js
@@ -3,7 +3,6 @@ import '../styles/style.css';
 import keyboardConfig from '../data/keyboard';
 
 (function () {
-  const lang = 'en';
   //
   // class LocalStorage {
   //   constructor() {
@@ -16,8 +15,18 @@ import keyboardConfig from '../data/keyboard';
       this.selector = selector;
       this.keysSet = {};
       this.isShiftPressed = false;
+      this.isCapsButtonPressed = false;
+      this.isAltPressed = false;
+      this.langs = ['en', 'ru'];
+      this.currentLayoutIndex = 0;
+
       this.buttonPressedClass = 'pressed';
-      this.upperCaseOnClass = 'case-on';
+      this.upperCaseOnClass = 'keyboard--case-on';
+      this.layoutHiddenClass = 'keyboard-layout--hidden';
+    }
+
+    get lang() {
+      return this.langs[this.currentLayoutIndex];
     }
 
     createKeyboard() {
@@ -64,15 +73,19 @@ import keyboardConfig from '../data/keyboard';
     createKeyboardKeyButton(keyConfig) {
       const keyButton = document.createElement('span');
       const className = 'key-button';
-      let symbol;
+      let symbol = '';
 
       if (keyConfig.type === 'system') {
         symbol = keyConfig.value;
       } else {
-        symbol = `
-          <span class="case-off">${keyConfig.values[lang][0]}</span>
-          <span class="case-on">${keyConfig.values[lang][1]}</span>
-        `;
+        for (let lang in keyConfig.values) {
+          symbol += `
+            <span class="keyboard-layout ${this.lang != lang ? this.layoutHiddenClass : ''}" data-lang="${lang}">
+              <span class="case-off">${keyConfig.values[lang][0]}</span>
+              <span class="case-on">${keyConfig.values[lang][1]}</span>
+            </span>
+          `;
+        }
       }
 
       keyButton.innerHTML = symbol;
@@ -134,14 +147,14 @@ import keyboardConfig from '../data/keyboard';
 
       switch (key.config.type) {
         case 'default':
-          this.outputValue(key.config.values[lang][letterCase]);
-          debugger;
+          this.outputValue(key.config.values[this.lang][letterCase]);
+
           if (this.isShiftPressed) {
             this.isShiftPressed = false;
             document.querySelector(`.${this.buttonPressedClass}`).classList.remove(this.buttonPressedClass);
             document.querySelector('.keyboard').classList.remove(this.upperCaseOnClass);
           }
-          
+
           break;
 
         case 'system':
@@ -176,23 +189,57 @@ import keyboardConfig from '../data/keyboard';
     }
 
     shift(key) {
-      console.log(key);
       this.isShiftPressed = !this.isShiftPressed;
 
       if (this.isShiftPressed) {
-        this.isShiftPressed = true;
+        // this.isShiftPressed = true;
         this.keyboard.classList.add(this.upperCaseOnClass);
         key.button.classList.add(this.buttonPressedClass);
       } else {
-        this.isShiftPressed = false;
+        // this.isShiftPressed = false;
         this.keyboard.classList.remove(this.upperCaseOnClass);
         key.button.classList.remove(this.buttonPressedClass);
+      }
+    }
+
+    alt(key) {
+      this.isAltPressed = !this.isAltPressed;
+
+      if (this.isAltPressed && this.isShiftPressed) {
+        key.button.classList.add(this.buttonPressedClass);
+        this.switchKeyboardLayout();
       }
     }
 
     resolveCase(isUp, btn) {
       if (isUp) {
       }
+    }
+
+    switchKeyboardLayout() {
+      this.currentLayoutIndex++;
+      this.currentLayoutIndex = this.currentLayoutIndex >= this.langs.length ? 0 : this.currentLayoutIndex;
+
+      this.keyboard
+        .querySelectorAll('.keyboard-layout')
+        .forEach(elem => {
+          if (elem.dataset.lang === this.lang) {
+            elem.classList.remove(this.layoutHiddenClass);
+          } else {
+            elem.classList.add(this.layoutHiddenClass);
+          }
+        });
+
+      this.keyboard
+        .querySelectorAll(`.${this.buttonPressedClass}`)
+        .forEach(button => {
+          button.classList.remove(this.buttonPressedClass);
+        });
+
+      this.keyboard.classList.remove(this.upperCaseOnClass);
+
+      this.isShiftPressed = false;
+      this.isAltPressed = false;
     }
 
     init() {

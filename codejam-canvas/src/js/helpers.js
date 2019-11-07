3438d58b6bbf572cdd5c345db4bfeb3986cd2197
fainz777 <fainz777@gmail.com>
2019-11-07 17:07:31 +0300

    refactor: pull-request issues

--- codejam-canvas/src/js/helpers.js
+++ codejam-canvas/src/js/helpers.js
@@ -1,12 +1,19 @@
 export default class Helpers {
 	static getColor(color) {
+		const defaultColor = '#000';
+
 		if (typeof color === 'string') {
 			return '#' + color;
 		}
 
 		let rgbaColor = [...color];
-		rgbaColor[3] = rgbaColor.length === 4 ? (rgbaColor[3] / 255).toFixed(2) : 1;
-		rgbaColor = `rgba(${rgbaColor.join(',')})`;
+
+		try {
+			rgbaColor[3] = rgbaColor.length === 4 ? (rgbaColor[3] / 255).toFixed(2) : 1;
+			rgbaColor = `rgba(${rgbaColor.join(',')})`;
+		} catch(e) {
+			return defaultColor
+		}
 
 		return rgbaColor;
 	}
